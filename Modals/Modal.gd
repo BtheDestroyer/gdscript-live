3b1673dc9ea4d2b9c40079941898e494a790b37f
Bryce Dixon <bryce@brycedixon.dev>
2024-05-31 23:11:18 -0700

    Opening a modal now focuses its close button; hid 'profile progress' text by default

--- Modals/Modal.gd
+++ Modals/Modal.gd
@@ -4,6 +4,7 @@ class_name Modal extends Control
 
 func _ready() -> void:
   close_button.pressed.connect(_on_close_pressed)
+  close_button.grab_focus()
 
 func _on_close_pressed() -> void:
   var modals := get_parent() as Modals
