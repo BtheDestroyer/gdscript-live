8097a67162fb5464595eb2c5156dce6ff60a1067
Bryce "BtheDestroyer" Dixon <bryce@brycedixon.dev>
2024-10-03 01:22:15 +0000

    Added links and copyright info to header; Build list is now manually written to allow for specific ordering of versions

--- endpoints/api/versions.js
+++ endpoints/api/versions.js
@@ -11,16 +11,14 @@ module.exports = {
     register_endpoints: async endpoints => {
         endpoints["/api/versions"] = async (req, res) => {
             try {
-                const {stdout, stderr} = await execAsync(`docker images | grep -e "^gdscript-live/godot\\s" | grep -ve "\\sbase\\s"`);
-                api.sendResponse(res, 200, {error:"", versions:`${stdout}`
-    .split("\n")
-    .map(line => line.substr("gdscript-live/godot".length).trim())
-    .map(line => line.substr(0, line.indexOf(' ')))
-    .filter(line => line.trim().length > 0)});
+                const versions = fs.readFileSync(path.join(process.cwd(), "containers/builds.txt"), {encoding: "utf-8", flag: "r"})
+                    .split("\n")
+                    .filter(line => line.trim().length > 0);
+                api.sendResponse(res, 200, {error:"", versions});
             }
             catch (error)
             {
-                api.sendResponse(res, 502, {error:"Failed to get available versions"});
+                api.sendResponse(res, 502, {error:`Failed to get available versions: ${error}`});
             }
             return true;
         };
